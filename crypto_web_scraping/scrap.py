f658c7fa995492dbfdba4d7eaa124d463ac16f04
thomas lemoine <tomlemoine@msn.com>
2022-07-03 22:32:10 +0200

    created first function to get all info

--- crypto_web_scraping/scrap.py
+++ crypto_web_scraping/scrap.py
@@ -1,18 +1,16 @@
-from operator import ge
-from matplotlib.pyplot import get
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import os
+import time
 
-def create_driver():
+def create_driver(url='https://finance.yahoo.com/topic/crypto/'):
     '''
-    Create driver from url "https://finance.yahoo.com/topic/crypto/"
-    and accept cookies
+    Create driver
+    By default url =  "https://finance.yahoo.com/topic/crypto/"
     '''
-    url = 'https://finance.yahoo.com/topic/crypto/'
     chrome_options = Options()
     chrome_options.add_argument("--headless") # Ensure GUI is off
     chrome_options.add_argument("--no-sandbox")
@@ -31,32 +29,134 @@ def create_driver():
 
     driver.get(url)
 
-    driver.find_element(By.XPATH, '//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button[1]').click()
-
-    #html_page = driver.page_source.encode("utf-8")
-
     return driver
 
 def get_basic_info(driver):
+    '''
+    From 'https://finance.yahoo.com/topic/crypto/' get basic informations
+    '''
+    # Accept Cookies
+    driver.find_element(By.XPATH, '//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button[1]').click()
+    driver.implicitly_wait(15)
+
+
+    # Scroll down to get 20 articles
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    SCROLL_PAUSE_TIME = 0.5
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # Scrap articles
     articles = driver.find_elements(By.CLASS_NAME, 'Ov\(h\).Pend\(44px\).Pstart\(25px\)')
     titles = []
     summaries = []
     sources = []
+    links = []
     for article in articles:
         titles.append(article.find_element(By.TAG_NAME, "a").text)
         summaries.append(article.find_element(By.TAG_NAME, "p").text)
-        sources.append(article.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+        sources.append(article.find_element(By.TAG_NAME, "span").text)
+        links.append(article.find_element(By.TAG_NAME, 'a').get_attribute('href'))
 
+    # Create df to be returned
     df = pd.DataFrame.from_dict({
         'TITLE':titles,
         'SUMMARY':summaries,
-        'SOURCE':sources
+        'SOURCE':sources,
+        'LINK':links
+        })
+
+    return df
+
+def get_more_info(driver):
+    '''
+    From 'https://finance.yahoo.com/topic/crypto/' get basic informations
+    '''
+
+    # Accept Cookies
+    driver.find_element(By.XPATH, '//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button[1]').click()
+    driver.implicitly_wait(15)
+
+
+    # Scroll down to get 20 articles
+
+    # # Get scroll height
+    # last_height = driver.execute_script("return document.body.scrollHeight")
+
+    # SCROLL_PAUSE_TIME = 0.5
+
+    # while True:
+    #     # Scroll down to bottom
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    #     # Wait to load page
+    #     time.sleep(SCROLL_PAUSE_TIME)
+
+    #     # Calculate new scroll height and compare with last scroll height
+    #     new_height = driver.execute_script("return document.body.scrollHeight")
+    #     if new_height == last_height:
+    #         break
+    #     last_height = new_height
+
+    # Scrap articles
+    articles = driver.find_elements(By.CLASS_NAME, 'Ov\(h\).Pend\(44px\).Pstart\(25px\)')
+    titles = []
+    summaries = []
+    sources = []
+    links = []
+    dates = []
+    full_texts = []
+    for article in articles:
+        titles.append(article.find_element(By.TAG_NAME, "a").text)
+        summaries.append(article.find_element(By.TAG_NAME, "p").text)
+        sources.append(article.find_element(By.TAG_NAME, "span").text)
+        links.append(article.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
+    for id, title in enumerate(titles):
+        article = driver.find_element(By.LINK_TEXT, title).click()
+        print(f"article {id} OK")
+        try:
+            driver.find_element(By.XPATH, '//article/div/div/div/div/div/div[2]/div[3]/div[2]/button').click()
+        except:
+            pass
+
+        dates.append(driver.find_element(By.TAG_NAME, 'time').get_attribute('datetime'))
+        full_texts.append(driver.find_element(By.CLASS_NAME, "caas-body").text)
+        driver.back()
+        driver.implicitly_wait(5)
+
+    driver.quit()
+
+    # Create df to be returned
+    df = pd.DataFrame.from_dict({
+        'TITLE':titles,
+        'SUMMARY':summaries,
+        'SOURCE':sources,
+        'LINK':links,
+        'DATE':dates,
+        'FULL_TEXT':full_texts
         })
 
     return df
 
 
+
 if __name__ == "__main__":
     driver = create_driver()
-    df  = get_basic_info(driver)
+    df = get_basic_info(driver)
+    df = append_full_info(df)
     print(df)
